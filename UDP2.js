7f2d1a03590d09a45f046414174911262d6103e3
Ubuntu <ubuntu@ip-172-26-0-244.eu-north-1.compute.internal>
2022-07-24 17:34:13 +0000

    minor changes

--- UDP2.js
+++ UDP2.js
@@ -11,18 +11,18 @@ let io          = socketIO(server);
 
 socket.on("message", (msg, rinfo) =>{
     io.emit("newMessage", msg.toString());
-    console.log(`server got ${msg} from ${rinfo.address}: ${rinfo.port}`);
+    console.log("server got ${msg}");
 });
 
-socket.bind(8080, "192.168.0.4", () => {
-    console.log("TCP socket formed on port 8080");
+socket.bind(3000, "127.0.0.1", () => {
+    console.log("TCP socket formed on port 3000");
 });
 
 const publicPath    = path.join(__dirname, "./public");
 app.use(express.static(publicPath));
 
-server.listen(3000, "127.0.0.1", () =>{
-    console.log("HTTP server listening on port 3000");
+server.listen(80, "127.0.0.1", () =>{
+    console.log("HTTP server listening on port 80");
 });
 
 io.on("connection", (socket) => {
